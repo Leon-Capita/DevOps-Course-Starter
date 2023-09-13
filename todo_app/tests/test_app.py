4af0813b58721183788df744ee8bd79c3cb1e8f5
Leon Milligan <leon.milligan@capita.com>
2023-09-13 10:57:56 +0100

    all working M2 and M3 as far as I remember (3 weeks ago!)

--- todo_app/tests/test_app.py
+++ todo_app/tests/test_app.py
@@ -1,18 +1,17 @@
 import os
 import pytest
 import requests
-from todo_app import app
+from todo_app.app import create_app, test_func
 from dotenv import load_dotenv, find_dotenv
 from todo_app.data.config import TRELLO_BOARD_ID
 from todo_app.debug import debug
-from flask import current_app, request ##################???????????????
 
 # file_path = find_dotenv('.env.test')
 # load_dotenv(file_path, override=True)
 
 @pytest.fixture
 def client():
-    with current_app.app_context(): ##################???????????????
+
         context = 'test_app.py pytest fixture'
         doing = 'app.create_app()'
         # Use our test integration config instead of the 'real' version
@@ -22,12 +21,12 @@ def client():
         load_dotenv(file_path, override=True)
 
         # Create the new app.
-        #test_app = app.create_app()
-        test_app = current_app.create_app() ##################???????????????
+        test_app = create_app()
+        #test_app = current_app.create_app()
         debug(context, doing, 'test_app', test_app)
 
         # Use the app to create a test_client that can be used in our tests.
-        with test_app.test_client() as client: ##################???????????????
+        with test_app.test_client() as client: 
             yield client
 
 class StubResponse():
@@ -38,7 +37,8 @@ class StubResponse():
         return self.fake_response_data
 
 # Stub replacement for requests.get(url)
-def stub(url, params={}):
+#def stub(url, params={}, headers=None): #headers = { "Accept": "application/json" }
+def stub(method, url, params={}, headers=None): #headers = { "Accept": "application/json" }
     #test_board_id = os.environ.get('TRELLO_BOARD_ID')
     test_board_id = TRELLO_BOARD_ID
     fake_response_data = None
@@ -46,22 +46,20 @@ def stub(url, params={}):
         fake_response_data = [{
             'id': '123abc',
             'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card'}]
+            'cards': [{'id': '456', 'name': 'Test card', 'idList': '64ae6f476f946f8c451a5cb9'}]
         }]
         return StubResponse(fake_response_data)
-
+#items.append(Item(item['id'], item['name'], item['idList']))
     raise Exception(f'Integration test did not expect URL "{url}"')
 
 
 def test_index_page(monkeypatch, client):
-    with current_app.app_context(): ##################???????????????
         # Replace requests.get(url) with our own function
         #monkeypatch.setattr(requests, 'get', stub)
         monkeypatch.setattr(requests, 'request', stub)
 
         # Make a request to our app's index page
-        #response = client.get('/')
-        response = client.request('/') ##################???????????????
+        response = client.get('/')
 
         assert response.status_code == 200
         assert 'Test card' in response.data.decode()
